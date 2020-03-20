7db5b773240be7c8be57376bb5c53dba9c8fae8b
scrumpygeo <scrumpygeo@yahoo.ca>
2020-03-20 11:53:40 +0000

    first commit

---
+++ src/components/Searchbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Searchbar extends Component {
+  render() {
+    return (
+      <div>
+<form>
+    <input type="text"/>
+</form>
+      </div>
+    );
+  }
+}
+
+export default Searchbar;
